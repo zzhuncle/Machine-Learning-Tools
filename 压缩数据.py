539427d582a4cf16e113f24e19404c3ba4cb4ede
zzhuncle <57178872+zzhuncle@users.noreply.github.com>
2022-07-23 20:20:57 +0800

    Create 压缩数据.py

---
+++ 压缩数据.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm  
+
+class _Data_Preprocess:
+    def __init__(self):
+        self.int8_max = np.iinfo(np.int8).max
+        self.int8_min = np.iinfo(np.int8).min
+
+        self.int16_max = np.iinfo(np.int16).max
+        self.int16_min = np.iinfo(np.int16).min
+
+        self.int32_max = np.iinfo(np.int32).max
+        self.int32_min = np.iinfo(np.int32).min
+
+        self.int64_max = np.iinfo(np.int64).max
+        self.int64_min = np.iinfo(np.int64).min
+
+        self.float16_max = np.finfo(np.float16).max
+        self.float16_min = np.finfo(np.float16).min
+
+        self.float32_max = np.finfo(np.float32).max
+        self.float32_min = np.finfo(np.float32).min
+
+        self.float64_max = np.finfo(np.float64).max
+        self.float64_min = np.finfo(np.float64).min
+
+    def _get_type(self, min_val, max_val, types):
+        if types == 'int':
+            if max_val <= self.int8_max and min_val >= self.int8_min:
+                return np.int8
+            elif max_val <= self.int16_max <= max_val and min_val >= self.int16_min:
+                return np.int16
+            elif max_val <= self.int32_max and min_val >= self.int32_min:
+                return np.int32
+            return None
+
+        elif types == 'float':
+            if max_val <= self.float16_max and min_val >= self.float16_min:
+                return np.float16
+            if max_val <= self.float32_max and min_val >= self.float32_min:
+                return np.float32
+            if max_val <= self.float64_max and min_val >= self.float64_min:
+                return np.float64
+            return None
+
+    def _memory_process(self, df):
+        init_memory = df.memory_usage().sum() / 1024 ** 2 / 1024
+        print('Original data occupies {} GB memory.'.format(init_memory))
+        df_cols = df.columns
+
+          
+        for col in tqdm_notebook(df_cols):
+            try:
+                if 'float' in str(df[col].dtypes):
+                    max_val = df[col].max()
+                    min_val = df[col].min()
+                    trans_types = self._get_type(min_val, max_val, 'float')
+                    if trans_types is not None:
+                        df[col] = df[col].astype(trans_types)
+                elif 'int' in str(df[col].dtypes):
+                    max_val = df[col].max()
+                    min_val = df[col].min()
+                    trans_types = self._get_type(min_val, max_val, 'int')
+                    if trans_types is not None:
+                        df[col] = df[col].astype(trans_types)
+            except:
+                print(' Can not do any process for column, {}.'.format(col)) 
+        afterprocess_memory = df.memory_usage().sum() / 1024 ** 2 / 1024
+        print('After processing, the data occupies {} GB memory.'.format(afterprocess_memory))
+        return df
